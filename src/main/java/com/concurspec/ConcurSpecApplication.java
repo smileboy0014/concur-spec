61e43433cbe97a592f2aafa9211b8ad329673389
gt.park <smileboy0014@gmail.com>
2025-12-29 22:48:22 +0900

    first commit

---
+++ src/main/java/com/concurspec/ConcurSpecApplication.java
@@ -0,0 +1,13 @@
+package com.concurspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurSpecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurSpecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/concurspec/ConcurSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurSpecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
