21297cbc7c7441a8225e33c8e7b0f0467e74c590
gt.park <smileboy0014@gmail.com>
2026-01-17 17:09:22 +0900

    chore: 테스트명 통일

--- concur-spec-junit5/src/test/java/io/github/concurspec/junit5/ConcurrentContextTest.java
+++ concur-spec-junit5/src/test/java/io/github/concurspec/junit5/ConcurrentContextTest.java
@@ -1,5 +1,6 @@
 package io.github.concurspec.junit5;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,7 +9,8 @@ public class ConcurrentContextTest {
 
     @Test
     @ConcurrentTest(threads = 10, duration = "300ms", warmup = "0ms")
-    void repeat_호출시_통계가_생성된다(ConcurrentContext ctx) throws InterruptedException {
+    @DisplayName("repeat 호출시 통계가 생성된다")
+    void shouldCreateStatWhenCallContext(ConcurrentContext ctx) throws InterruptedException {
         AtomicInteger i = new AtomicInteger();
         ctx.repeat(i::incrementAndGet);
 
--- examples/basic-usage/src/test/java/ConcurrentExTest.java
+++ examples/basic-usage/src/test/java/ConcurrentExTest.java
@@ -1,5 +1,6 @@
 import io.github.concurspec.junit5.ConcurrentContext;
 import io.github.concurspec.junit5.ConcurrentTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -9,8 +10,9 @@ public class ConcurrentExTest {
     private final AtomicInteger counter = new AtomicInteger();
 
     @Test
+    @DisplayName("증가 동시성 테스트")
     @ConcurrentTest(threads = 30, duration = "500ms", warmup = "100ms", maxPendingFailures = 5)
-    void 증가_동시성_테스트(ConcurrentContext ctx) throws InterruptedException {
+    void shouldIncreaseCountConcurrently(ConcurrentContext ctx) throws InterruptedException {
         ctx.repeat(counter::incrementAndGet);
 
         ctx.assertNoUncaughtErrors();
